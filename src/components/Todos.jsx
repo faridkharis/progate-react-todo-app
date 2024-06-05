a54afe0034948504e2fe361e12e4e094eff3e357
Muhamad Farid Kharismawan <faridkharismawan@gmail.com>
2024-06-05 16:52:46 +0700

    create Todos and TodoItem component

---
+++ src/components/Todos.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+const Todos = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} />
+      })}
+    </div>
+  )
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array
+};
+
+export default Todos;
