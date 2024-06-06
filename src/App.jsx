14ef890cda5f04babec1ffb481d4a43157377b06
Muhamad Farid Kharismawan <faridkharismawan@gmail.com>
2024-06-06 09:19:47 +0700

    Develop delete todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -21,22 +21,31 @@ function App() {
     },
   ]);
 
-  // console.log(todos);
-
   const toggleCompleted = (todoId) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === todoId) {
         todo.completed = !todo.completed;
       }
       return todo;
-    })
+    });
+    setTodos(updatedTodos);
+  }
+
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => (
+      todo.id !== todoId
+    ));
     setTodos(updatedTodos);
   }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} toggleCompleted={toggleCompleted} />
+      <Todos
+        todos={todos}
+        toggleCompleted={toggleCompleted}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
